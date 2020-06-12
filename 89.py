5c15256fc92c80649203ba46c1f9f156dfaf987c
sumitvashist <skvd07@gmail.com>
2020-06-12 21:32:07 +0530

    new pattern

---
+++ 89.py
@@ -0,0 +1,20 @@
+
+n = int(input("Enter a number : "))
+
+for i in range(n): 
+	for j in range(i + 1, n): 
+		print(' ', end = '') 
+	for j in range(0, 2 * i + 1): 
+		print('*', end = '') 
+	print() 
+for i in range(n//2): 
+	for j in range(n//2): 
+		print('*', end = '') 
+	for j in range(2 * n - 2 *(n//2) -1): 		
+		print(' ', end = '') 
+	for j in range(n//2): 
+		print('*', end = '') 
+	print() 
+
+
+
